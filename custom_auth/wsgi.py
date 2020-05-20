50cb095c22dd90c7a4e80e2be341bcd9e23d68f5
repavn <repavn@gmail.com>
2020-05-20 20:00:04 +0400

    move settings

--- custom_auth/wsgi.py
+++ custom_auth/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'custom_auth.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'custom_auth.settings.local_settings')
 
 application = get_wsgi_application()
