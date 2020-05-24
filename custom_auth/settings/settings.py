e5bc50ce1cdbb82fc3f6610f2c2afd8970e06836
repavn <repavn@gmail.com>
2020-05-24 09:21:45 +0400

    connect to postgres

--- custom_auth/settings/settings.py
+++ custom_auth/settings/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'custom_auth.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inetlead',
+        'USER': 'inetleaduser',
+        'PASSWORD': os.environ.get('PG_PASS'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
